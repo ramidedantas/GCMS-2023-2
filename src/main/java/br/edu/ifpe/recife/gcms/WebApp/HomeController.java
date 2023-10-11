1ae7fa0b40f911ae6ea1347b93054ec24b52a4b0
Ramide Dantas <ramidedantas@recife.ifpe.edu.br>
2023-10-11 16:58:40 -0300

    1o commit

---
+++ src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
@@ -0,0 +1,20 @@
+package br.edu.ifpe.recife.gcms.WebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public @ResponseBody String greeting() {
+		return "Hello, World";
+	}
+	
+	@RequestMapping("/fulano")
+	public @ResponseBody String greetingFulano() {
+		return "Hello, Fulano!";
+	}
+
+}
---
+++ src/test/java/br/edu/ifpe/recife/gcms/WebApp/WebAppApplicationTests.java
@@ -0,0 +1,42 @@
+package br.edu.ifpe.recife.gcms.WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/"))
+				.andDo(print()).andExpect(status().isOk())
+				.andExpect(content()
+						.string(containsString("Hello, World")));
+	}
+	
+	@Test
+	public void novoTeste() throws Exception {
+		this.mockMvc.perform(get("/fulano"))
+				.andDo(print()).andExpect(status().isOk())
+				.andExpect(content()
+						.string(containsString("Hello, Fulano")));
+	}
+}
