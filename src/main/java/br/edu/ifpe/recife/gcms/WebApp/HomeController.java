fc5a33b9d0db96fb414d118c00955b9e5eeffb47
Ramide Dantas <ramidedantas@recife.ifpe.edu.br>
2023-10-18 11:22:57 -0300

    Nova modificação

--- src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
+++ src/main/java/br/edu/ifpe/recife/gcms/WebApp/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
 	@RequestMapping("/")
 	public @ResponseBody String greeting() {
-		return "Hello, World";
+		return "Hello, World!!!!";
 	}
 	
 	@RequestMapping("/fulano")
