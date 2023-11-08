87c045c3ed7355ea4310ed314717b94f3b97dfb4
Ramide Dantas <ramidedantas@recife.ifpe.edu.br>
2023-11-08 11:21:20 -0300

    Jenkins: bug

--- src/test/java/br/edu/ifpe/recife/gcms/WebApp/WebAppApplicationTests.java
+++ src/test/java/br/edu/ifpe/recife/gcms/WebApp/WebAppApplicationTests.java
@@ -27,7 +27,7 @@ class WebAppApplicationTests {
 	@Test
 	public void shouldReturnDefaultMessage() throws Exception {
 		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().string(containsString("Hello, World")));
+				.andExpect(content().string(containsString("Hello, World???")));
 	}
 
 	@Test
